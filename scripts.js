fba74bd0eba22929bcc39b69f8d87cee8f787279
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-05-03 19:19:01 +0200

    Exercise done

--- scripts.js
+++ scripts.js
@@ -1,1 +1,39 @@
 console.log(document.title);
+
+// 1
+let gen1 = document.getElementById("gen-1");
+gen1.innerText = "Generasión 1 Pokimon";
+
+// 2
+let gen1List = document.querySelector(".infocard-list");
+gen1List.style.background = "#c0c0f0";
+
+// 3
+console.log(document.URL);
+
+// 4
+console.log(location.host);
+
+// 5
+let images = document.querySelectorAll("img");
+console.log(images);
+
+// 6
+images.forEach((n) => {
+  // n.src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+  n.setAttribute(
+    "src",
+    "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+  );
+});
+
+// Extra
+let cards = document.querySelectorAll(".infocard-lg-data.text-muted");
+// let cards = document.querySelectorAll(".itype.flying");
+// cards.forEach(
+//   (n) => (n.parentNode.parentNode.style.background = "#c0f0c0")
+// );
+
+cards.forEach((n) => {
+  if (n.querySelector(".itype.flying")) n.style.background = "#c0f0c0";
+});
